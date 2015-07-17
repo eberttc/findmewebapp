84cfd8996188ce120e9f7d75258a6b85d83bd408
Kenji Oki <kenoki@live.com>
2015-07-17 16:35:35 +0000

    Se ejecutó el commando annotate. En el layout base se enlazó los links con las rutas correspondientes

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id          :integer          not null, primary key
+#  comment     :text(65535)
+#  founded     :integer
+#  lost_pet_id :integer
+#  user_id     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
